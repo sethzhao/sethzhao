1cb16b81fbf97b5564454ae5b924cd45bb5ace39
set.zhao <slert@qq.com>
2021-05-12 21:06:06 +0800

    markdown loader

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,22 +1,15 @@
 <template>
   <div>
-    <section class="markdown" v-html="state.content" />
+    <section class="markdown">
+      <About />
+    </section>
   </div>
 </template>
 
 <script setup>
-import MarkdownIt from 'markdown-it'
+import About from '../markdown/about.md'
 import { defineProps, reactive } from 'vue'
 
-const md = new MarkdownIt()
-var content = md.render(`
-我是一名全栈Web开发者，生活在上海。
-
-`);
-
-let state = reactive({
-  content
-})
 
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
+import Markdown from 'vite-plugin-md'
 
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown(),
     styleImport({
       libs: [{
         libraryName: 'element-plus',
